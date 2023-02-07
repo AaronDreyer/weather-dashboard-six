82726e2c9a8b4e1446aaacea8f03adc27163124c
Aaron Dreyer <aaronottaway@gmail.com>
2023-02-07 11:50:39 -0500

    Variables/Arrays and API Call
    Set up variables for search hsitory and city data to save and pull from local storage. I also set the variables for the api url and key with an accompanying function to retreieve data.

---
+++ Develop/script.js
@@ -0,0 +1,30 @@
+// Arrays for current and past cities entered
+let searchHistory = []
+let cityData = ""
+// Variables for API URLs and API key
+var initialUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=";
+var apiKey = "5ea5a9949ddb1597c11f87fc3d143ea6";
+var coordUrl = "https://api.openweathermap.org/data/2.5/uvi?lat=";
+
+// API call to OpenWeather
+let getWeather = function(city) {
+    // Format for OpenWeather API
+    // Entered "city" to retrieve city data
+    let apiUrl = initialUrl + city + "&appid=" + apiKey + "&units=imperial";
+
+// Fetch the data from API call
+    fetch(apiUrl)
+        
+    // Function to display city data 
+    // Accessing response and if response is "ok" (completed) then weather will be displayed
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    // Future function
+                    displayWeather(data);
+                });
+
+            }
+        })  
+};
