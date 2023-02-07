7c65da224be94163cbc746857982274ea227a4f7
Aaron Dreyer <aaronottaway@gmail.com>
2023-02-07 12:52:17 -0500

    Five day forecast Display
    I added the necessary function to display the 5 day forecast along with the requirements for each forecast day including icon, temp, humidity, wind speed, and date. I also applied a method to save the search history to the website and local storage.

--- Develop/script.js
+++ Develop/script.js
@@ -48,3 +48,62 @@ let formSubmit = function(event) {
         $("#city").val("");
     } 
 };
+
+// Created a function that I mentioned above to display the weather data according to entered city from the weather api
+let displayWeather = function(weatherData) {
+
+    // Set up for current day weather information and date
+    // displays city name and current date using dayjs
+    // Append weather icon based on current day ([0]) that matches with openweather api current weather
+    $("#city-name").text(weatherData.name + " (" + dayjs(weatherData.dt * 1000).format("M/D/YYYY") + ") ").append(`<img src="https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png"></img>`);
+    // Displays current day temperature
+    // toFixed() is a Number method that is used to convert a number to fixed-point notation (rounding the result where necessary) and return its value as a string
+    $("#city-temp").text("Temperature: " + weatherData.main.temp.toFixed(1) + " °F");
+    // Displays current day humidity
+    $("#city-humid").text("Humidity: " + weatherData.main.humidity + "%");
+    // Displays current day wind speed
+    $("#city-wind").text("Wind Speed: " + weatherData.wind.speed.toFixed(1) + " mph");
+
+// Fetching different url for weather forecast for next 5 days using longitude and latitude
+    fetch(forecastUrl + weatherData.name + "&appid=" + apiKey + "&units=imperial")
+        .then(function(response) {
+            // Retrieves response data and implements into function
+            response.json().then(function(data) {
+        
+
+                // Clears 5 day forecast section
+                $("#five-day").empty();
+
+                // This took me forever to figure out so this is going to be a long explanation
+                /* The openweather api's 5 day forecast bases the 5 days as a value of 40 (Im assuming its based on a 40 hour work day so 9 hours a day).
+                 In order to equally spread the current weather day and the next 5 days within that 40 hour span I had to figure out what increments to use. 
+                 You cannot start at 0-6 as the current day will show twice (current day and the first day of the 5 day forecast) so I needed to start at 7.
+                 From there I needed to find an increment to evenly spread across 40 without going over because going past 40 will cause the last day to not appear.
+                 The increment of 8 starting at 7 ends the 5 day span at 39 which displays 5 days of weather */
+                for(i = 7; i <= data.list.length; i += 8){
+            
+                    // This displays the 5 day weather forecast by adding a div within the section alloted for the forecast
+                    // This allows the the forecast to appear rather than be on the page automatically
+                    let fiveDayCard =
+                    // Displays date using dayjs, weather icon like on current weather day, temperature, wind speed, and humidity
+                        `<div>
+                            <h5>` + dayjs(data.list[i].dt * 1000).format("M/D/YYYY") + `</h5>
+                            <img src="https://openweathermap.org/img/wn/` + data.list[i].weather[0].icon + `.png" alt="Weather Icon">
+                            <p>Temp: ` + data.list[i].main.temp + " °F" + `</p>
+                            <p>Humidity: ` + data.list[i].main.humidity + `</p>
+                            <p>Wind: ` + data.list[i].wind.speed + " mph" + `</p>
+                        </div>`;
+
+                    // Appends the forecast card to the id
+                    $("#five-day").append(fiveDayCard);
+               }
+            })
+        });
+
+    // Saves the last city searched
+    cityData = weatherData.name;
+
+    // Saves the search history using openweather api's name value
+    saveHistoryData(weatherData.name);
+};
+
